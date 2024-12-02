92550ae87b0f92fb33b961776e8478c3bb7d4d2b
Andrei Olea <andrei@andreiolea.com>
2024-12-02 13:02:44 -0500

    📝 chore(gitignore): add node_modules to .gitignore 🚀 feat(vscode): add custom color settings for VS Code
    🎨 add(appily-logo): include horizontal black SVG logo assets
    
    🎨 add(appily-pattern-teal.svg): add teal pattern SVG asset
    
    📦 chore(appily-vector-shapes.svg): add new SVG file with vector shapes for Appily website
    
    ✨ feat(index.html, main.js): add initial HTML and JavaScript content for Vite App
    
    ✨ feat(package.json): add initial package.json configuration for flexreg-styles project
    
    ✨ feat(buttons.scss): add styles for custom button component
    
    📝 chore(tokens): add color and typography tokens to _tokens.scss
    📝 feat(utilities): add shadow mixin using color tokens from _tokens.scss
    
    ✨ feat(styles.scss): add global styles for the application including layout, typography, and form elements

---
+++ main.js
@@ -0,0 +1,31 @@
+import './styles/styles.scss'
+
+document.querySelector('#app').innerHTML = `
+<main>
+  <div class="container">
+    <div class="progress">1 / 12</div>
+    <h2>The next step in my educational journey is earning a degree related...</h2>
+    <ul class="options">
+      <li><label><input type="radio" name="degree"><span> Undergraduate degree</span></label></li>
+      <li><label><input type="radio" name="degree"><span> Master's degree</span></label></li>
+      <li><label><input type="radio" name="degree"><span> Professional doctorate or PhD</span></label></li>
+      <li><label><input type="radio" name="degree"><span> Undergraduate degree, as a transfer student</span></label></li>
+      <li><label><input type="radio" name="degree"><span> I want to earn a degree that's not in education</span></label></li>
+      <li><label><input type="radio" name="degree"><span> I'm not interested in a degree right now</span></label></li>
+    </ul>
+    <button class="button btn-primary">
+      <div class="btn-content">
+        <span class="btn-label">Next</span>
+      </div>
+    </button>
+    <figure class="appily-logo">
+      <img src="assets/appily-logo-horizontal-black.svg" alt="Appily Advance logo" />
+    </figure>
+  </div>
+</main>
+  <footer class="page-footer">
+      <p>Terms and Conditions · Privacy Policy · California Privacy Notice · Do Not Sell or Share My Personal Information · Privacy Preferences</p>
+    </footer>
+`
+
+setupCounter(document.querySelector('#counter'))
